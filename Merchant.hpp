88b66d822330f0b7ef0e988882e686459af628a7
Wahylian <42039207+Wahylian@users.noreply.github.com>
2025-05-11 18:33:24 +0300

    Delete Merchant.hpp

--- Merchant.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Player.hpp"
-namespace coup{
-    class Merchant: public Player{
-        public:
-            // constructor
-            Merchant(Game &game, const string &name);
-    };
-}
